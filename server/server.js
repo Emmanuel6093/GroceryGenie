61f6c5005fbf0f493b3824671c19d660705892e0
Emmanuel6093 <elorenzo6093@gmail.com>
2025-02-10 16:39:38 -0500

    adding models

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("GroceryGenie Backend is Running!");
