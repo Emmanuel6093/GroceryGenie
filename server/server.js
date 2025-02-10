d66e04aa4c8459ecb2d1fb149d0f78a747c12b86
Emmanuel6093 <elorenzo6093@gmail.com>
2025-02-10 16:37:16 -0500

    Server set up

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("GroceryGenie Backend is Running!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB ✅"))
+  .catch((error) => console.error("MongoDB Connection Error:", error));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
